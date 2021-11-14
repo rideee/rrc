729f6e24af83563d82aab8521fc9a44a3dc53094
rideee <michal.katnik91@gmail.com>
2021-11-14 17:10:56 +0100

    unused variables removed

--- init.bash
+++ init.bash
@@ -16,10 +16,7 @@ export RRC_COMPLETIONS_DIR="$RRC_DIR/completions"
 export RRC_CONF_DIR="$RRC_DIR/conf.d"
 export RRC_FUNC_DIR="$RRC_DIR/func.d"
 export RRC_MAN_DIR="$RRC_DIR/man"
-export RRC_SRC_DIR="$RRC_DIR/src"
-export RRC_TESTS_DIR="$RRC_DIR/tests"
 export RRC_USR_DIR="$RRC_DIR/usr.d"
-export RRC_VAR_DIR="$RRC_DIR/var"
 
 # Add bin dir to PATH.
 [[ $(echo $PATH | grep "$RRC_BIN_DIR") ]] || export PATH="$PATH:$RRC_BIN_DIR"
