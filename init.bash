09c9100626b6b7e32371de4c07b6662035d301b9
rideee <michal.katnik91@gmail.com>
2021-11-14 15:21:24 +0100

    import custom user conf files added

--- init.bash
+++ init.bash
@@ -18,8 +18,10 @@ export RRC_FUNC_DIR="$RRC_DIR/func.d"
 export RRC_MAN_DIR="$RRC_DIR/man"
 export RRC_SRC_DIR="$RRC_DIR/src"
 export RRC_TESTS_DIR="$RRC_DIR/tests"
-export RRC_USR_DIR="$RRC_DIR/usr"
+export RRC_USR_DIR="$RRC_DIR/usr.d"
 export RRC_VAR_DIR="$RRC_DIR/var"
+
+# Add bin dir to PATH.
 [[ $(echo $PATH | grep "$RRC_BIN_DIR") ]] || export PATH="$PATH:$RRC_BIN_DIR"
 
 # Import func files.
@@ -27,6 +29,9 @@ for f in $RRC_FUNC_DIR/*; do [[ -f $f ]] && . $f; done
 
 # Import conf files.
 for f in $RRC_CONF_DIR/*; do [[ -f $f ]] && . $f; done
+
+# import custom user configuration files, best place for overriding defaults.
+for f in $RRC_USR_DIR/*; do [[ -f $f ]] && . $f; done
 
 # Import completion files.
 for f in $RRC_COMPLETIONS_DIR/*; do [[ -f $f ]] && . $f; done
---
+++ usr.d/custom.bash
@@ -0,0 +1,4 @@
+# Custom
+
+# Set PS1.
+PROMPT_COMMAND='rrc::ps1 simplify'
