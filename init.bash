37b5ac09ff583e467c0d4a55301167e6c8e41cf5
rideee <michal.katnik91@gmail.com>
2021-11-19 21:11:49 +0100

    "check if in interactive mode" uncommented

--- init.bash
+++ init.bash
@@ -7,7 +7,7 @@
 #################################################
 
 # If not running interactively, don't do anything.
-# [[ $- != *i* ]] && return   #TODO: Uncomment this line.
+[[ $- != *i* ]] && return
 
 # Set the rrc directories.
 export RRC_DIR=$(dirname ${BASH_SOURCE[0]})
