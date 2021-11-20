0fec37c8f3e8355ca18fb1b2448f29b64399f20e
rideee <michal.katnik91@gmail.com>
2021-11-20 22:25:10 +0100

    Renamed to drop-in convention

--- init.bash
+++ init.bash
@@ -15,7 +15,7 @@ export RRC_DIR=$(dirname ${BASH_SOURCE[0]})
 
 # RRC_DIR related.
 export RRC_BIN_DIR="$RRC_DIR/bin"
-export RRC_COMPLETIONS_DIR="$RRC_DIR/completions"
+export RRC_COMPLETION_DIR="$RRC_DIR/completion.d"
 export RRC_CONF_DIR="$RRC_DIR/conf.d"
 export RRC_FUNC_DIR="$RRC_DIR/func.d"
 export RRC_MAN_DIR="$RRC_DIR/man"
@@ -40,4 +40,4 @@ for f in $RRC_USR_DIR/*; do [[ -f $f ]] &&
   . $f; done
 
 # Import completion files.
-for f in $RRC_COMPLETIONS_DIR/*; do [[ -f $f ]] && . $f; done
+for f in $RRC_COMPLETION_DIR/*; do [[ -f $f ]] && . $f; done
