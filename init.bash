9779ee7b753255ae59e3e8f24830f067eea45098
rideee <michal.katnik91@gmail.com>
2021-11-20 09:07:02 +0100

    RRC_SHARED_DIR added

--- init.bash
+++ init.bash
@@ -9,15 +9,21 @@
 # If not running interactively, don't do anything.
 [[ $- != *i* ]] && return
 
-# Set the rrc directories.
+# Set required environment variables.
+# RRC_DIR is a root directory.
 export RRC_DIR=$(dirname ${BASH_SOURCE[0]})
+
+# RRC_DIR related.
 export RRC_BIN_DIR="$RRC_DIR/bin"
 export RRC_COMPLETIONS_DIR="$RRC_DIR/completions"
 export RRC_CONF_DIR="$RRC_DIR/conf.d"
 export RRC_FUNC_DIR="$RRC_DIR/func.d"
 export RRC_MAN_DIR="$RRC_DIR/man"
 export RRC_USR_DIR="$RRC_DIR/usr.d"
+
+# RRC_SRC_DIR related.
 export RRC_SRC_DIR="$RRC_DIR/src"
+export RRC_SHARED_DIR="$RRC_SRC_DIR/rrc/shared"
 
 # Add bin dir to PATH.
 [[ $(echo $PATH | grep "$RRC_BIN_DIR") ]] || export PATH="$PATH:$RRC_BIN_DIR"
