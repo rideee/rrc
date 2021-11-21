e1e0a06386c8be492546103ed0b33c2ab2f1dc22
rideee <michal.katnik91@gmail.com>
2021-11-21 14:59:31 +0100

    shellcheck's fixes

--- init.bash
+++ init.bash
@@ -10,7 +10,8 @@
 [[ $- != *i* ]] && return
 
 # RRC_DIR stores path to root rrc directory.
-export RRC_DIR=$(dirname ${BASH_SOURCE[0]})
+export RRC_DIR
+RRC_DIR=$(dirname "${BASH_SOURCE[0]}")
 
 # RRC_FUNC_DIR stores path to func.d drop-in directory.
 export RRC_FUNC_DIR="$RRC_DIR/func.d"
@@ -28,19 +29,25 @@ export RRC_COMPLETION_DIR="$RRC_DIR/completion.d"
 export RRC_BIN_DIR="$RRC_DIR/bin"
 
 # Add bin directory to PATH.
-[[ $(echo $PATH | grep "$RRC_BIN_DIR") ]] || export PATH="$PATH:$RRC_BIN_DIR"
+if echo "$PATH" | grep -q "$RRC_BIN_DIR"; then
+  export PATH="$PATH:$RRC_BIN_DIR"
+fi
 
 # Import function files.
-for f in $RRC_FUNC_DIR/*; do [[ -f $f ]] && . $f; done
+# shellcheck source=/dev/null
+for f in "$RRC_FUNC_DIR"/*; do [[ -f $f ]] && . "$f"; done
 
 # Import confg files.
-for f in $RRC_CONF_DIR/*; do [[ -f $f ]] && . $f; done
+# shellcheck source=/dev/null
+for f in "$RRC_CONF_DIR"/*; do [[ -f $f ]] && . "$f"; done
 
 # Import custom users config files.
 # The best place for overwriting defaults.
-for f in $RRC_USR_DIR/*; do [[ -f $f ]] &&
-  [[ $(basename $f) != 'README.md' ]] &&
-  . $f; done
+# shellcheck source=/dev/null
+for f in "$RRC_USR_DIR"/*; do [[ -f $f ]] &&
+  [[ $(basename "$f") != 'README.md' ]] &&
+  . "$f"; done
 
 # Import completion files.
-for f in $RRC_COMPLETION_DIR/*; do [[ -f $f ]] && . $f; done
+# shellcheck source=/dev/null
+for f in "$RRC_COMPLETION_DIR"/*; do [[ -f $f ]] && . "$f"; done
