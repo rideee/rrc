059c905ebf60aa13d476ed8a92afe6f511bc33ec
rideee <michal.katnik91@gmail.com>
2021-11-21 04:46:50 +0100

    customization of fzf options via _fzf_comprun function added

--- func.d/fzf-key-bindings.bash
+++ func.d/fzf-key-bindings.bash
@@ -16,6 +16,22 @@ fzf::printKeyBindings() {
   head -9 "$RRC_FUNC_DIR/fzf-key-bindings.bash" | awk '{gsub("# ",""); gsub("#",""); print}'
 }
 
+# (EXPERIMENTAL) Advanced customization of fzf options via _fzf_comprun function
+# - The first argument to the function is the name of the command.
+# - You should make sure to pass the rest of the arguments to fzf.
+_fzf_comprun() {
+  local command=$1
+  shift
+
+  case "$command" in
+  grep) fzf "$@" --preview 'tree -C {} | head -200' ;;
+  cd) fzf "$@" --preview 'tree -C {} | head -200' ;;
+  export | unset) fzf "$@" --preview "eval 'echo \$'{}" ;;
+  ssh) fzf "$@" --preview 'dig {}' ;;
+  *) fzf "$@" ;;
+  esac
+}
+
 # ---
 
 __fzf_select__() {
