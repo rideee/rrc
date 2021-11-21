c7228835282bbad967e0d382bd3049201eab762a
rideee <michal.katnik91@gmail.com>
2021-11-21 12:42:04 +0100

    Default prompt style changed

--- func.d/ps1.bash
+++ func.d/ps1.bash
@@ -5,15 +5,16 @@
 function rrc::ps1() {
   local ps1_errCode="$?"
 
-  local ps1_ecArrow="\[\033[38;5;2m\]√\[$(tput sgr0)\]" # Exit code 0 arrow
+  local ps1_exitCode="\[\033[38;5;2m\]√\[$(tput sgr0)\]" # Exit code 0 arrow
   [ "$ps1_errCode" != 0 ] &&
-    ps1_ecArrow="\[\033[38;5;1m\]ε:${ps1_errCode}\[$(tput sgr0)\]" # Exit code !0 arrow
+    ps1_exitCode="\[\033[38;5;1m\]ε:${ps1_errCode}\[$(tput sgr0)\]" # Exit code !0 arrow
 
   local ps1_gitArrow="\[\033[38;5;86m\]↳\[$(tput sgr0)\]"
-  local ps1_arrow="\[\033[38;5;4m\]➜\[$(tput sgr0)\]"
-  local ps1_usr="\[$(tput bold)\]\[\033[38;5;12m\]\u\[$(tput sgr0)\]"
-  local ps1_host="\[$(tput bold)\]\[\033[38;5;12m\]\h\[$(tput sgr0)\]"
-  local ps1_path="\[\033[38;5;11m\]\W\[$(tput sgr0)\]"
+  local ps1_separator="\[\033[38;5;4m\]@\[$(tput sgr0)\]"
+  local ps1_usr="\[\033[38;5;12m\]\u\[$(tput sgr0)\]"
+  local ps1_host="\[\033[38;5;12m\]\h\[$(tput sgr0)\]"
+  local ps1_path="\[\033[38;5;11m\]\w\[$(tput sgr0)\]"
+  local ps1_dirname="\[\033[38;5;11m\]\W\[$(tput sgr0)\]"
   local ps1_promChar="\[$(tput bold)\]\[\033[38;5;12m\]\$\[$(tput sgr0)\]"
 
   # Check if root.
@@ -22,7 +23,7 @@ function rrc::ps1() {
     ps1_promChar="\[$(tput bold)\]\[\033[38;5;9m\]\$\[$(tput sgr0)\]"
   fi
 
-  local ps1_usr_host="${ps1_host}${ps1_arrow} ${ps1_usr}"
+  local ps1_usr_host="${ps1_usr}${ps1_separator}${ps1_host}"
 
   # If git in PATH.
   if [ "$(type git 2>/dev/null)" ]; then
@@ -38,9 +39,9 @@ function rrc::ps1() {
     fi
   fi
 
-  # TODO: It is a good idea to implement configuration flags.
+  # TODO: Implement themes.
   # If $1 == simplify then remove user and hostname.
   [[ $1 == 'simplify' ]] &&
-    export PS1="${ps1_ecArrow}${ps1_git} ${ps1_path} ${ps1_promChar} " ||
-    export PS1="${ps1_ecArrow}${ps1_git} ${ps1_usr_host} ${ps1_path} ${ps1_promChar} "
+    export PS1="${ps1_exitCode}${ps1_git} ${ps1_dirname} ${ps1_promChar} " ||
+    export PS1="${ps1_exitCode}${ps1_git} ${ps1_usr_host} ${ps1_path} ${ps1_promChar} "
 }
